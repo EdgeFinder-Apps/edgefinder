89867cddcb8532e2d7373ee399a4da1db198245d
Ben Greenberg <ben@yalladevrel.com>
2025-11-22 21:20:24 -0300

    Add EVVMSandboxRegistry contract scaffolding

---
+++ contracts/EVVMSandboxRegistry.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract EVVMSandboxRegistry {
+    uint256 public constant EVVM_ID = 2;
+    address public constant MATE_EVVM_ADDRESS = 0x9902984d86059234c3B6e11D5eAEC55f9627dD0f;
+}
